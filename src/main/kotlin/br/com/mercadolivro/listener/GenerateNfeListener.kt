e63c18350b4c0b2e358c57d1f21beeb2b1bd557b
josue.messias <jmessiasweb>
2022-09-09 10:14:39 -0300

    implementacão da classe GenerateNfListener

---
+++ src/main/kotlin/br/com/mercadolivro/listener/GenerateNfeListener.kt
@@ -0,0 +1,19 @@
+package br.com.mercadolivro.listener
+
+import br.com.mercadolivro.service.PurchaseService
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import java.util.*
+
+
+@Component
+class GenerateNfeListener(private val purchaseService: PurchaseService) {
+
+    @EventListener
+    fun listen(purchaseEvent: PurchaseEvent) {
+        val nfe = UUID.randomUUID().toString()
+        val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
+        purchaseService.update(purchaseModel)
+
+    }
+}
--- src/main/kotlin/br/com/mercadolivro/service/PurchaseService.kt
+++ src/main/kotlin/br/com/mercadolivro/service/PurchaseService.kt
@@ -1,14 +1,23 @@
 package br.com.mercadolivro.service
 
+import br.com.mercadolivro.listener.PurchaseEvent
 import br.com.mercadolivro.model.PurchaseModel
 import br.com.mercadolivro.repositoy.PurchaseRepository
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
-class PurchaseService(private val purchaseRepositoy: PurchaseRepository) {
+class PurchaseService(private val purchaseRepositoy: PurchaseRepository,
+                      private val applicationEventPublisher: ApplicationEventPublisher) {
 
     fun create(purchaseModel: PurchaseModel) {
         purchaseRepositoy.save(purchaseModel)
 
+        applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
+
+    }
+
+    fun update(purchaseModel: PurchaseModel) {
+        purchaseRepositoy.save(purchaseModel)
     }
 }
