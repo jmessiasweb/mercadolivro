ce3e3115a37158c556da91616926dbc15c34cb6e
josue.messias <jmessiasweb>
2022-09-15 17:20:51 -0300

    implementação da classe SecurityConfig

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -12,9 +13,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 @EnableWebSecurity
 class SecurityConfig(): WebSecurityConfigurerAdapter() {
 
+    private  val PUBLIC_MATCHERS = arrayOf<String>()
+
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+        "/customer"
+    )
+
     override fun configure(http: HttpSecurity) {
         http.cors().and().csrf().disable()
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+            .antMatchers(*PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            .anyRequest().authenticated()
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
