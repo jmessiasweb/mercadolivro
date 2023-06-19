16382ec916999bed81165bbbe22a38c6f8ff8257
josue.messias <jmessiasweb>
2023-06-19 10:02:34 -0300

    atualização de classes

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -3,12 +3,11 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.controller.response.CustomerResponse
-import com.mercadolivro.extension.toCustomerModel
-import com.mercadolivro.extension.toResponse
+import com.mercadolivro.extesion.toCustomerModel
+import com.mercadolivro.extesion.toResponse
 import com.mercadolivro.security.UserCanOnlyAccessTheirOwnResource
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
